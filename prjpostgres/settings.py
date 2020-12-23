8f15d5d5950e7c316527feeb1698e820482a3c67
mayuragile <mayur.barbhaya@agileinfoways.com>
2020-12-23 12:50:01 +0530

    'added dbsqlite3'

--- prjpostgres/settings.py
+++ prjpostgres/settings.py
@@ -73,14 +73,21 @@ WSGI_APPLICATION = 'prjpostgres.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
